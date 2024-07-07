6408f7c09f8e2d360370ff04a88755da0b3247c6
SumitKumar-17 <sumitkumar@kgpian.iitkgp.ac.in>
2024-07-07 14:43:35 +0530

    .

--- src/app/api/format/route.ts
+++ src/app/api/format/route.ts
@@ -1,13 +1,14 @@
 import { formatSchema } from "@prisma/internals";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method Not Allowed" });
     return;
   }
 
   const schema = req.body.schema as string;
+  //@ts-ignore
   const formatted = await formatSchema({ schema });
 
   res.json({ formatted });
