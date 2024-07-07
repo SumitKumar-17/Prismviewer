0e256d9de202879af2f4bc22bf1adb406b630900
SumitKumar-17 <sumitkumar@kgpian.iitkgp.ac.in>
2024-07-07 11:04:54 +0530

    feat:adding info for the prisma push

--- src/components/AlertDetails.tsx
+++ src/components/AlertDetails.tsx
@@ -9,7 +9,7 @@ import {
 
 export function AlertDetails() {
   return (
-    //@ts-ignore
+    <>
     <Alert>
       <Terminal className="h-4 w-4" />
       <AlertTitle>Do not use online !</AlertTitle>
@@ -17,5 +17,14 @@ export function AlertDetails() {
         You can use this online for that you will be needing to open a specific port of your system to the internet,which is risky.
       </AlertDescription>
     </Alert>
+    <Alert>
+      <Terminal className="h-4 w-4" />
+      <AlertTitle>For pushing the schema </AlertTitle>
+      <AlertDescription>
+       Take your prisma.schema and paste into the prisma folder of this project.
+       Then click on the Push button.
+      </AlertDescription>
+    </Alert>
+    </>
   )
 }
